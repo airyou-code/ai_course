a17a82b06c8ea25b7e01646bbe397b14e1570a06
Artem Kirillov <artem.k3001@gmail.com>
2024-01-20 18:26:03 +0100

    init frontend

---
+++ src/frontend/src/components/ListGroup.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup(props: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1)
+
+  return (
+    <>
+      <h1>{ props.heading }</h1>
+      <ul className="list-group">
+        {props.items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={index}
+            onClick={() => {
+              selectedIndex === index
+                ? setSelectedIndex(-1)
+                : setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
