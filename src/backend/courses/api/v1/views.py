4bb9ad40a25e00f04c4062ec2889bd366b291d70
Artem Kirillov <artem.k3001@gmail.com>
2025-02-02 16:46:57 +0100

    Refactor LessonContentBlocksViewSet to improve code readability and consistency; update block type checks and response formatting.

--- src/backend/courses/api/v1/views.py
+++ src/backend/courses/api/v1/views.py
@@ -3,7 +3,11 @@ from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from .serializers import GroupSerializer, ContentBlockSerializer, ContentBlockListSerializer
+from .serializers import (
+    GroupSerializer,
+    ContentBlockSerializer,
+    ContentBlockListSerializer,
+)
 from rest_framework.authentication import SessionAuthentication
 
 
@@ -13,12 +17,11 @@ class GroupReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
     authentication_classes = [JWTAuthentication, SessionAuthentication]
 
     def get_queryset(self):
-        return Group.objects.prefetch_related('module_set__lesson_set')
+        return Group.objects.prefetch_related("module_set__lesson_set")
 
 
 class LessonContentBlocksViewSet(
-    viewsets.GenericViewSet,
-    viewsets.mixins.ListModelMixin
+    viewsets.GenericViewSet, viewsets.mixins.ListModelMixin
 ):
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [JWTAuthentication, SessionAuthentication]
@@ -28,41 +31,37 @@ class LessonContentBlocksViewSet(
         try:
             lesson = Lesson.objects.get(uuid=lesson_uuid)
         except Lesson.DoesNotExist:
-            return Response({'error': 'Lesson not found'}, status=404)
+            return Response({"error": "Lesson not found"}, status=404)
 
-        content_blocks = ContentBlock.objects.filter(lesson=lesson).order_by('order')
+        content_blocks = ContentBlock.objects.filter(lesson=lesson).order_by("order")
         blocks = []
+
+        next_lesson = (
+            Lesson.objects.filter(module=lesson.module, order__gt=lesson.order)
+            .order_by("order")
+            .first()
+        )
 
         for block in content_blocks:
             content = None
-            if block.block_type in ['text', 'output_dialog']:
+            if block.block_type in ["text", "output_dialog"]:
                 content = block.content_html
-            elif block.block_type in ['choices_field', 'test']:
+            elif block.block_type in ["choices_field", "test"]:
                 content = block.content_json
             else:
                 content = block.content_text
 
-            blocks.append({
-                'type': block.block_type,
-                'content': content,
-                'avatar': block.avatar if hasattr(block, 'avatar') else None,
-                'nextLessonUrl': block.nextLessonUrl if hasattr(block, 'nextLessonUrl') else None,
-            })
+            next_lesson_url = None
+            if block.block_type == "button_next" and next_lesson:
+                next_lesson_url = f"/lesson/{next_lesson.uuid}"
 
-        return Response({'blocks': blocks})
+            blocks.append(
+                {
+                    "type": block.block_type,
+                    "content": content,
+                    "avatar": block.avatar if hasattr(block, "avatar") else None,
+                    'nextLessonUrl': next_lesson_url,
+                }
+            )
 
-    # def create(self, request, lesson_uuid: str = None):
-    #     serializer = self.get_serializer(
-    #         data=request.data,
-    #     )
-    #     serializer.is_valid(raise_exception=True)
-    #     validated_data = serializer.validated_data
-
-    #     blocks_data = validated_data.get('blocks')
-
-    #     lesson = Lesson.objects.get(uuid=lesson_uuid)
-    #     for block_data in blocks_data:
-    #         ContentBlock.objects.create(lesson=lesson, **block_data)
-
-    #     headers = self.get_success_headers(serializer.data)
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response({"blocks": blocks})
