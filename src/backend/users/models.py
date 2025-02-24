f9f9edfeb9c57ac71fbdf0e86ab4405046f64617
Artem Kirillov <artem.k3001@gmail.com>
2025-02-24 17:01:17 +0100

    Add UserLessonProgress model to track user lesson progress and last seen content block

--- src/backend/users/models.py
+++ src/backend/users/models.py
@@ -1,6 +1,7 @@
 from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from datetime import datetime
 
 class CourseUser(AbstractUser):
@@ -21,3 +22,13 @@ class CourseUser(AbstractUser):
         return self.subscription_set.filter(
             end_date__gte=datetime.now()
         ).first()
+
+
+class UserLessonProgress(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    lesson = models.ForeignKey('courses.Lesson', on_delete=models.CASCADE)
+    last_seen_block = models.ForeignKey('courses.ContentBlock', on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'lesson')
