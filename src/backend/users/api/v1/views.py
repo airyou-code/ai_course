db296ffe8dd01934860514c0e1ed444a6ac3a2a7
Artem Kirillov <artem.k3001@gmail.com>
2025-02-27 16:55:45 +0100

    Rename project from Trader Note to AI Course; update related documentation and settings; add UserLessonProgress model and API endpoints for tracking user lesson progress

--- src/backend/users/api/v1/views.py
+++ src/backend/users/api/v1/views.py
@@ -1,8 +1,11 @@
 from users.models import CourseUser
-from rest_framework import viewsets, permissions
-from .serializers import UserSerializer
+from rest_framework import viewsets, permissions, generics, status
+from .serializers import UserSerializer, UserLessonProgressSerializer, BlockUUIDSerializer
+from courses.models import Lesson, ContentBlock
 # from rest_framework.authentication import SessionAuthentication
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.response import Response
+from users.models import UserLessonProgress
 
 
 class UserReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
@@ -15,3 +18,36 @@ class UserReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         return CourseUser.objects.filter(id=self.request.user.id)
+
+
+class UpdateProgressView(generics.UpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = UserLessonProgress.objects.all()
+    serializer_class = UserLessonProgressSerializer
+
+    def update(self, request, *args, **kwargs):
+        block_uuid = request.data.get('last_seen_block_uuid')
+        content_block = ContentBlock.objects.filter(uuid=block_uuid).first()
+        UserLessonProgress.objects.get_or_create(
+            user=request.user, lesson=content_block.lesson,
+            defaults={'last_seen_block': content_block}
+        )
+        return Response(status=status.HTTP_200_OK)
+
+
+class GetProgressAPIView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = BlockUUIDSerializer
+
+    def get(self, request, lesson_uuid, *args, **kwargs):
+        try:
+            lesson = Lesson.objects.get(uuid=lesson_uuid)
+        except Lesson.DoesNotExist:
+            return Response({"error": "Lesson not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        progress = UserLessonProgress.objects.filter(user=request.user, lesson=lesson).first()
+        if not progress or not progress.last_seen_block:
+            return Response({"error": "Progress not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.get_serializer(progress)
+        return Response(serializer.data, status=status.HTTP_200_OK)
